ab7cb38b406d6b69674edc443e8800aed860c6fd
ArjunaRus <arjuna_na@mail.ru>
2018-04-05 02:32:22 +0700

    A new approach of data containers

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera(90, WIDTH / HEIGHT, 1, 200);
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial();
+
 camera.position.x = 3;
 camera.position.y = 3;
 camera.position.z = 10;
@@ -27,96 +28,96 @@ onWindowResize();
 
 controls = new THREE.OrbitControls(camera, renderer.domElement);
 
+function GetCubeColor()
+{
+    var rColor = (0x11).toString();
+    var gColor = (0x22).toString();
+    var bColor = Math.round(Math.random() * 140 + 50).toString();
+    return "rgba(" + rColor + "," + gColor + "," + bColor + ", 255)";
+}
+
 // - - - classes - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 class Cube {
-  // Numbers[], positions[][3]
-  // THREE.cube
-  constructor(x, y, z) {
-    //if (x === undefined) x = ...; //todo
-    var str = '#1122' + (Math.round(Math.random() * 140 + 50).toString(16));
-    // this.drawwable = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: '#' + Math.round(Math.random() * 156 + 100).toString(16) + Math.round(Math.random() * 156 + 100).toString(16) + Math.round(Math.random() * 156 + 100).toString(16) }));
-    this.drawwable = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: str }));
-    this.drawwable.position.set(x, y, z);
-    // generate Numbers
-
-    // picking teh numbers
-
-    this.numbers = [];
-    this.route = {
-      vector: [], //vector3
-      position: [], //vector3
-      currentIndex: 0
-    }
-
-    var xyz = [x, y, z]; // copy of args
-    for (var c = 0; c < xyz.length; c++) {
-      this.numbers.push([]);
-      for (var i = 0; i < 3; i++) {
-        this.numbers[c].push(Math.ceil(Math.random() * Math.min(9, xyz[c])));
-        xyz[c] -= this.numbers[c][this.numbers[c].length - 1];
-      }
-    }
-    // getting vectors
-    this.route.vector.push(new THREE.Vector3(this.numbers[0][0] - this.numbers[0][1], this.numbers[0][1] - this.numbers[0][2], this.numbers[0][2] - this.numbers[0][0]));
-    this.route.vector.push(new THREE.Vector3(this.numbers[1][0] - this.numbers[1][1], this.numbers[1][1] - this.numbers[1][2], this.numbers[1][0] - this.numbers[1][2]));
-    this.route.vector.push(new THREE.Vector3(this.numbers[2][0] - this.numbers[2][1], this.numbers[2][1] - this.numbers[2][2], this.numbers[2][0] - this.numbers[2][2]));
-
-    // getting routes
-    this.route.position.push(new THREE.Vector3(x, y, z).add(this.route.vector[0]));
-    this.route.position.push(this.route.position[this.route.position.length - 1].add(this.route.vector[1]));
-    this.route.position.push(this.route.position[this.route.position.length - 1].add(this.route.vector[2]));
-    console.log(this.route.vector);
-    console.log(this.route.position);
-    // todo: is error?
-
-    // numbers :: [[int]] -> str
-    for (var c = 0; c < xyz.length; c++)
-      this.numbers[c] = this.numbers[c].join('') + " ";
-    this.numbers = this.numbers.join('');
-    // console.log([x, y, z].join(",") + " = " + this.getNumbers());
-    // console.log(this);
-  }
-  getNumbers() {
-    return this.numbers;
-  }
-  getCoordinates() { }
-  getNextCoordinates() { }
-  move() { }
-  draw() {
-
-  }
-}
-class Hypercube { // cube manager & dispatcher
-  // cubes[SIZE+2][SIZE+2][SIZE+2]
-  // bridgeCube
-  draw() { // ← :/
-  }
-  constructor(spawnChance) {
-    this.cubes = [[], [], []];
-    for (var i = 1; i <= SIZE; i++) {
-      for (var j = 1; j <= SIZE; j++) {
-        for (var k = 1; k <= SIZE; k++) {
-          if (Math.random() > spawnChance)
-            continue;
-          this.cubes.push(new Cube(i, j, k));
-          scene.add(this.cubes[this.cubes.length - 1].drawwable);
-          // todo: nearbies detection
+    
+    static GetCoord(iteration, cubeNumber) {
+        
+        if (cubeNumber > 999 || cubeNumber < 0)
+            throw "Incorrect cube number" + cuberNumebr;
+        
+        // Cube has only 3 iterations
+        switch(iteration) {
+            case 0:
+                return cubeNumber % 10 + (cubeNumber/10) % 10 + (cubeNumber/100) % 10; 
+            case 1:
+                return 2 * ((cubeNumber/10) % 10) + (cubeNumber/100) % 10; 
+            case 2:
+                return (cubeNumber/10) % 10 + 2 *((cubeNumber/100) % 10);
+            default: throw "Incorrect cuber iteration: " + iteration;
         }
-      }
     }
-    this.bridgeCube = new Cube(Math.floor(SIZE / 2), Math.floor(SIZE / 2), SIZE + 1);
-    this.cubes.push(this.bridgeCube); // bridge
-    scene.add(this.bridgeCube.drawwable);
-  }
-  isMoveble(cube) { }
-  move(cube) { }
-  findPath() { }
+    
+    GetCubePosition(iteration) {
+        return [Cube.GetCoord(iteration, this.a),
+                Cube.GetCoord(iteration, this.b),
+                Cube.GetCoord(iteration, this.c)];
+    }
+    
+    // Cube is full-described by three numbers (aaa, bbb, ccc)
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        
+        this.position = this.GetCubePosition(0);
+        
+        this.drawable = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: GetCubeColor() }));
+        this.drawable.position.set(this.position[0], this.position[1], this.position[2]);
+    }
+    
+    getNumbers() {
+        return this.numbers;
+    }
+    
+    getCoordinates() { }
+    getNextCoordinates() { }
+    move() { }
+    draw() { }
+}
+
+var CUBES = [[566, 472, 737],
+             [656, 778, 462]
+             // Add cubes here
+            ]
+
+class Shell {
+    // TODO skeleton shell
+    constructor() {
+        this.drawable = new THREE.Mesh(new THREE.BoxGeometry(27, 27, 27), new THREE.MeshBasicMaterial())
+    }
+}
+            
+class Hypercube { 
+    draw() { }
+  
+    constructor() {
+        this.cubes = [];
+        for (var i = 0; i < CUBES.length; ++i) {
+            this.cubes.push(new Cube(CUBES[i][0], CUBES[i][1], CUBES[i][2]));
+            scene.add(this.cubes[this.cubes.length - 1].drawable);
+        }
+        
+        scene.add((new Shell()).drawable)
+    }
+    
+    isMoveble(cube) { }
+    move(cube) { }
+    findPath() { }
 }
 
 //- - - main - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-var hc = new Hypercube(0.45);
+var hc = new Hypercube();
 
 // - - - init 2/2
 
@@ -138,7 +139,7 @@ function onWindowResize() {
 }
 var render = function () {
   requestAnimationFrame(render);
-  //hc.cubes[hc.cubes.length-1].drawwable.rotation.y += 0.01;
+  //hc.cubes[hc.cubes.length-1].drawable.rotation.y += 0.01;
   //cube.rotation.y += 0.01;
   renderer.render(scene, camera);
   //controls.update();
